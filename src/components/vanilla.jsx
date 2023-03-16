4fd31b09ec2341b9b00e8a8a565d15a4b756e800
ArturoExe <amarturin15@gmail.com>
2023-03-16 15:18:58 -0700

    Style and first demo

--- src/components/vanilla.jsx
+++ src/components/vanilla.jsx
@@ -1,28 +1,46 @@
-import { useTeachable } from '../hooks/useTeachable'
+import { useEffect, useState } from "react";
+import useLetter from "../hooks/useLetter";
+import { useTeachable } from "../hooks/useTeachable";
+import CameraSkeleton from "./CameraSkeleton";
 
 export const Vanilla = () => {
   // More API functions here:
   // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
-
   // the link to your model provided by Teachable Machine export panel
-  const { init, stop, probabilities } = useTeachable("https://teachablemachine.withgoogle.com/models/DoT35G9In/")
+  const { init, stop, probabilities, isOn } = useTeachable(
+    "https://teachablemachine.withgoogle.com/models/DoT35G9In/"
+  );
+
+  const letter = useLetter(probabilities);
 
   return (
-    <div>
-      <button onClick={init}>
-        Start
-      </button>
-      <button onClick={stop}>
-        Stop
-      </button>
-      <div id="webcam-container"></div>
-      <ul>
+    <div className="vanilla-container">
+      <div>
+        <button onClick={init} className="btn-primary">
+          Start
+        </button>
+        <button onClick={stop} className="btn-secondary">
+          Stop
+        </button>
+      </div>
+
+      {!isOn && <CameraSkeleton />}
+      <div id="webcam-container" style={{ borderRadius: "30px" }}></div>
+      {/* <ul>
         {probabilities?.map(({ className, probability }) => (
-          <li key={className}>
-            {`${className}: ${probability.toFixed(2)}`}
-          </li>
+          <li key={className}>{`${className}: ${probability.toFixed(2)}`}</li>
         ))}
-      </ul>
+      </ul> */}
+
+      {isOn ? (
+        <h2 className="letter">
+          Detecting letter <span>{letter}</span>
+        </h2>
+      ) : (
+        <h4 style={{ fontWeight: "lighter" }}>
+          Let us start detecting some handsigns
+        </h4>
+      )}
     </div>
-  )
-}
+  );
+};
--- src/hooks/useTeachable.js
+++ src/hooks/useTeachable.js
@@ -1,65 +1,70 @@
-import * as tmImage from "@teachablemachine/image"
-import { useEffect, useState } from 'react'
+import * as tmImage from "@teachablemachine/image";
+import { useEffect, useState } from "react";
+import useTimer from "./useTimer";
 
 export const useTeachable = (URL) => {
-  const [probabilities, setProbabilities] = useState([])
-  const [model, setModel] = useState(null)
-  const [webcam, setWebcam] = useState(null)
-  const [stopped, setStopped] = useState(false)
-  const modelURL = URL + "model.json"
-  const metadataURL = URL + "metadata.json"
+  const [probabilities, setProbabilities] = useState([]);
+  const [model, setModel] = useState(null);
+  const [webcam, setWebcam] = useState(null);
+  const [stopped, setStopped] = useState(false);
+  const [isOn, setIsOn] = useState(false);
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
 
   useEffect(() => {
-    webcam &&
-      setupCamera()
-  }, [webcam])
+    webcam && setupCamera();
+  }, [webcam]);
 
-  async function setupCamera () {
-    await webcam.setup() // request access to the webcam
-    await webcam.play()
-    window.requestAnimationFrame(loop)
-
+  async function setupCamera() {
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+    setIsOn(true);
     // append elements to the DOM
-    document.getElementById("webcam-container").appendChild(webcam.canvas)
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
   }
 
   // Load the image model and setup the webcam
-  async function init () {
+  async function init() {
     // load the model and metadata
     // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
     // or files from your local hard drive
     // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-    setStopped(false)
-    setModel(await tmImage.load(modelURL, metadataURL))
+    setStopped(false);
+    setModel(await tmImage.load(modelURL, metadataURL));
 
     // Convenience function to setup a webcam
-    setWebcam(new tmImage.Webcam(200, 200, true)) // width, height, flip
+    setWebcam(new tmImage.Webcam(300, 300, true)); // width, height, flip
   }
 
-  async function loop () {
-    webcam.update() // update the webcam frame
-    await predict()
-    window.requestAnimationFrame(loop)
+  async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    console.log("loop");
+    window.requestAnimationFrame(loop);
   }
 
   // run the webcam image through the image model
-  async function predict () {
+  async function predict() {
     // predict can take in an image, video or canvas html element
-    const prediction = await model.predict(webcam.canvas)
-    setProbabilities(prediction)
+
+    const prediction = await model.predict(webcam.canvas);
+    setProbabilities(prediction);
   }
 
-  async function stop () {
-    await webcam.stop()
-    setProbabilities([])
-    setStopped(true)
-    document.getElementById("webcam-container")?.removeChild(webcam.canvas)
+  async function stop() {
+    await webcam.stop();
+    setProbabilities([]);
+    setStopped(true);
+    document.getElementById("webcam-container")?.removeChild(webcam.canvas);
+    setIsOn(false);
   }
 
   return {
     init,
     stop,
     stopped,
-    probabilities
-  }
-}
+    probabilities,
+    isOn,
+  };
+};
